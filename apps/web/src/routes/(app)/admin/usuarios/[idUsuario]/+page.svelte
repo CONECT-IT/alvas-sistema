4fa24066c0cc179506c974642a04d36ac02ee2d9
NexusOnePlus <wv35788@gmail.com>
2026-05-23 14:36:17 -0500

    chore: rastreo faltante de id

---
+++ apps/web/src/routes/(app)/admin/usuarios/[idUsuario]/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import UsuarioDetail from '$lib/usuarios/presentation/UsuarioDetail.svelte';
+
+	let idUsuario = $derived($page.params.idUsuario ?? '');
+</script>
+
+<svelte:head>
+	<title>Usuario {idUsuario} | ALVAS</title>
+</svelte:head>
+
+<UsuarioDetail {idUsuario} />
